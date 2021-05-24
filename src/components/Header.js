70bbc20e6563881fa6a6c29a2234610c1e5c3750
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-05-24 14:56:03 -0300

    feat: add home and header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <Container>
+            <h1>Header</h1>
+        </Container>
+    )
+}
+
+export default Header;
+
+const Container = styled.nav`
+
+`;
